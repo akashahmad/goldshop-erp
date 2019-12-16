83a811b9ff32646c466409c8fe3725942f12ed2d
hamzakhan747 <mhamzakhan2019@gmail.com>
2019-12-16 12:19:36 +0500

    modal customer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Container/Homepage/Homepage'
 import Section4  from './Components/Section4(customerHeader)/section4'
 import Model from './Components/modelButton/modelButton'
 import Addgold from './Components/modalButton(addgold)/addgold'
+import Addcustomer from './Components/modalButton(addcustomer)/addcustomer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assects/style/common.css'
 import './assects/style/style.css'
@@ -18,9 +19,9 @@ export default()=>{
           <Route path={"/home"} component={Home} />
           <Route path={"/signin"} component={signin}/>
           <Route path={"/section4"} component={Section4} />
-          <Route path={"/login"} component={LOGIN}/>
           <Route path={"/model"} component={Model}/>
           <Route path={"/addgold"} component={Addgold}/>
+          <Route path={"/addcustomer"} component={Addcustomer}/>
         </Switch>
     </BrowserRouter>
      </>
