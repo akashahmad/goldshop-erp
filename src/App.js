da5d0fc06f99c35731bed19970a7bc9624ccdc7e
adeelnazeer <adeelcomsats070@gmail.com>
2019-12-16 09:39:02 +0500

    section 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter , Route , Switch } from 'react-router-dom'
 import LOGIN from './Container/login/login'
 import Home from './Container/Homepage/Homepage'
+import Section4  from './Components/Section4(customerHeader)/section4'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assects/style/common.css'
 import './assects/style/style.css'
@@ -14,6 +15,7 @@ export default()=>{
         <Switch>
           <Route path={"/home"} component={Home} />
           <Route path={"/login"} component={LOGIN}/>
+          <Route path={"/section4"} component={Section4} />
         </Switch>
     </BrowserRouter>
      </>
---
+++ src/Components/Section4(customerHeader)/Style.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+export default ()=>{
+return(
+<style js="true">{`
+.Header-section2
+{
+    padding:0;
+    background-color: #f8f9fd;
+
+}
+
+.navbar-section2-dashboard{
+    padding:1%;
+  
+}
+ul.navbar-topnav-list.navbar-nav.ml-auto {
+    display: flex;
+    align-items: center;
+}
+.navbar-top{
+    margin-top: 18px;
+    padding-right: 4%;
+    display: flex;
+    justify-content: flex-end;
+}
+a.nav-link {
+    color: white;
+    opacity:70%;
+}
+.vertical-line-inside-navbar{
+    border-left: 1.5px solid white;
+    height: 28px;
+    margin-top: 3px;
+    padding-left: 6px;
+    opacity:50%;
+}
+.image-search{
+    // margin-top: 6px;
+    padding-right: 15px;
+}
+.text-navbar-search{
+    padding-left:15px;
+}
+.person-image-navbar{
+    height:50px;
+}
+.dashboard-content-homepage {
+    width: 100%;
+    background-image: linear-gradient(to top, #1483bc, #29a1df);
+    // background-color: #2094cf;
+    // min-height: 48vh;
+    transition: all 0.3s;
+    border-bottom-left-radius: 40px;
+    border-bottom-right-radius: 40px;
+}
+
+`}</style>
+
+
+
+);
+
+
+}
