d83560da9bde784367d1d6e6fe485401f943ffb5
unknown <ms335833@gmail.com>
2019-12-17 18:07:48 +0500

    Added

--- src/Container/CustomerViewMoney/customerViewmoney.js
+++ src/Container/CustomerViewMoney/customerViewmoney.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Section1 from '../../Components/Section1/section1'
-import Section4 from '../../Components/Section4(customerHeader)/section4'
+import customerTranscation from '../../Components/customerDetails(viewTransaction)/customerDetails'
 import '../../assects/style/common.css'
 
 export default ()=>{
     return(
         <>
-<div style={{display:"flex"}}>
-<Section1 />
-<Section4/>
-</div >
-</>
+            <div style={{display:"flex"}}>
+            <Section1 />
+            <customerTranscation/>
+            </div >
+        
+        </>
     );
 }
